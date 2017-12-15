e91e8399c66f9eff0b6d25f947b47a81507be8be
Sreelakshmi <srlkhmi@gmail.com>
2017-12-15 15:48:55 +0530

    Cleaned the code

--- helloworld_32_sys.asm
+++ helloworld_32_sys.asm
@@ -16,6 +16,5 @@ _start:                                         ;tell linker entry point
     mov     eax,1                               ;system call number (sys_exit)
     int     0x80                                ;call kernel
 
-section     .data
 
 
